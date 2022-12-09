5dc738ee0c77b74e86a5c221d9463e939b94320c
bqcuong <bqcuong2212@gmail.com>
2022-12-09 11:57:48 +0100

    day 06

---
+++ Day06/main.py
@@ -0,0 +1,19 @@
+lines = []
+with open('input', 'r') as f:
+    lines = f.read().splitlines()
+
+line = lines[0]
+
+# part 1
+for i in range(3, len(line)):
+    if len(line[i-3:i+1]) == len(set(line[i-3:i+1])):
+        print(line[i-3:i+1])
+        print(i + 1)
+        break
+
+# part 2
+for i in range(13, len(line)):
+    if len(line[i-13:i+1]) == len(set(line[i-13:i+1])):
+        print(line[i-13:i+1])
+        print(i + 1)
+        break
