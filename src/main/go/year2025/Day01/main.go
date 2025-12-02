e0e70340ac0832fa9f3498f12bbb0ccd7d8dd1c5
David Bui <david.bui@freiheit.com>
2025-12-02 21:50:59 +0100

    Day 01+02 Year 2025

---
+++ src/main/go/year2025/Day01/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func readTextFile(path string) ([]string, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	return strings.Split(string(data), "\n"), nil
+}
+
+func main() {
+	//lines, err := readTextFile("sample_input")
+	lines, err := readTextFile("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	points := 50
+	points2 := 50
+	count := 0
+	count2 := 0
+	for _, line := range lines {
+		rotation := line[0]
+		step, _ := strconv.Atoi(line[1:])
+		if rotation == 'L' {
+			points -= step
+		} else {
+			points += step
+		}
+		points = (points + 100) % 100
+		if points == 0 {
+			count++
+		}
+
+		for range step {
+			if rotation == 'L' {
+				points2--
+			} else {
+				points2++
+			}
+			if points2%100 == 0 {
+				count2++
+			}
+		}
+	}
+
+	fmt.Println(count, count2)
+}
---
+++ src/main/go/year2025/Day02/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func readTextFile(path string) ([]string, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	return strings.Split(string(data), "\n"), nil
+}
+
+func isRepeatedTwice(id string) bool {
+	return len(id)%2 == 0 && id[:len(id)/2] == id[len(id)/2:]
+}
+
+func isRepeated(id string) bool {
+	for subLen := 1; subLen <= len(id)/2; subLen++ {
+		if len(id)%subLen != 0 {
+			continue
+		}
+
+		i := 0
+		for ; i <= len(id)-subLen; i += subLen {
+			if id[i:i+subLen] != id[:subLen] {
+				break
+			}
+		}
+		if i == len(id) {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	//lines, err := readTextFile("sample_input")
+	lines, err := readTextFile("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	idPairs := strings.Split(lines[0], ",")
+
+	sum, sum2 := 0, 0
+	for _, pairStr := range idPairs {
+		pair := strings.Split(pairStr, "-")
+		firstId, _ := strconv.Atoi(pair[0])
+		sndId, _ := strconv.Atoi(pair[1])
+
+		for id := firstId; id <= sndId; id++ {
+			if isRepeatedTwice(strconv.Itoa(id)) {
+				sum += id
+			}
+			if isRepeated(strconv.Itoa(id)) {
+				sum2 += id
+			}
+		}
+	}
+
+	fmt.Println(sum, sum2)
+}
