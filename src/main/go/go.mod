7509050c98b8ca34da8bea1daed6a27f256bd989
David Bui <david.bui@freiheit.com>
2025-12-10 16:26:46 +0100

    update

--- src/main/go/go.mod
+++ src/main/go/go.mod
@@ -1,3 +1,5 @@
 module github.com/bqcuong/AdventOfCode
 
 go 1.24.3
+
+require github.com/aclements/go-z3/z3 v0.0.0-20220809013456-4675d5f90ca5
--- src/main/go/year2025/Day10/main.go
+++ src/main/go/year2025/Day10/main.go
@@ -1,116 +1,121 @@
 package main
 
 import (
-	"container/list"
-	"fmt"
-	"slices"
-	"strconv"
-	"strings"
+    "container/list"
+    "fmt"
+    "slices"
+    "strconv"
+    "strings"
 
-	c "github.com/bqcuong/AdventOfCode/common"
+    c "github.com/bqcuong/AdventOfCode/common"
 )
 
 type Day10 struct{}
 
 type Button struct {
-	ToggledIndicators []int
+    ToggledIndicators []int
 }
 
 type Config struct {
-	States          []bool
-	FinalStates     []bool
-	Buttons         []Button
-	RequiredJoltage []int
+    States          []bool
+    FinalStates     []bool
+    Buttons         []Button
+    RequiredJoltage []int
 }
 
 func (d Day10) Solve(part c.Part, lines []string) int {
-	configs := d.ReadConfigs(lines)
-	if part == c.PART1 {
-		sum := 0
-		for _, config := range configs {
-			sum += d.getFewestButtonPresses(config)
-		}
-		return sum
-	}
-	return 0
+    configs := d.ReadConfigs(lines)
+    sum := 0
+    for _, config := range configs {
+        if part == c.PART1 {
+            sum += d.countButtonPressesForStates(config)
+        } else {
+            sum += d.countButtonPressesForJoltages(config)
+        }
+    }
+    return sum
 }
 
 type QueueElement struct {
-	States []bool
-	depth  int
+    States []bool
+    depth  int
 }
 
-func (d Day10) getFewestButtonPresses(config Config) int {
-	queue := list.New()
-	queue.PushBack(QueueElement{config.States, 0})
+func (d Day10) countButtonPressesForJoltages(config Config) int {
+    return 0
+}
 
-	visitedStates := make(map[string]bool)
-	visitedStates[fmt.Sprintf("%v", config.States)] = true
+func (d Day10) countButtonPressesForStates(config Config) int {
+    queue := list.New()
+    queue.PushBack(QueueElement{config.States, 0})
 
-	// implementation of BFS with queue
-	for queue.Len() > 0 {
-		current := queue.Front().Value.(QueueElement)
-		queue.Remove(queue.Front())
+    visitedStates := make(map[string]bool)
+    visitedStates[fmt.Sprintf("%v", config.States)] = true
 
-		if slices.Equal(current.States, config.FinalStates) {
-			return current.depth
-		}
-		for _, button := range config.Buttons {
-			newStates := make([]bool, len(current.States))
-			copy(newStates, current.States)
-			for _, toggledIndicator := range button.ToggledIndicators {
-				newStates[toggledIndicator] = !newStates[toggledIndicator]
-			}
+    // implementation of BFS with queue
+    for queue.Len() > 0 {
+        current := queue.Front().Value.(QueueElement)
+        queue.Remove(queue.Front())
 
-			stateStr := fmt.Sprintf("%v", newStates)
-			if _, ok := visitedStates[stateStr]; !ok {
-				visitedStates[stateStr] = true
-				queue.PushBack(QueueElement{newStates, current.depth + 1})
-			}
-		}
-	}
-	return -1
+        if slices.Equal(current.States, config.FinalStates) {
+            return current.depth
+        }
+        for _, button := range config.Buttons {
+            newStates := make([]bool, len(current.States))
+            copy(newStates, current.States)
+            for _, toggledIndicator := range button.ToggledIndicators {
+                newStates[toggledIndicator] = !newStates[toggledIndicator]
+            }
+
+            stateStr := fmt.Sprintf("%v", newStates)
+            if _, ok := visitedStates[stateStr]; !ok {
+                visitedStates[stateStr] = true
+                queue.PushBack(QueueElement{newStates, current.depth + 1})
+            }
+        }
+    }
+    return -1
 }
 
 func (d Day10) ReadConfigs(lines []string) []Config {
-	configs := make([]Config, 0)
-	for _, line := range lines {
-		parts := strings.Fields(line)
+    configs := make([]Config, 0)
+    for _, line := range lines {
+        parts := strings.Fields(line)
 
-		finalStates := make([]bool, 0)
-		for _, s := range []rune(parts[0][1 : len(parts[0])-1]) {
-			if s == '#' {
-				finalStates = append(finalStates, true)
-			} else {
-				finalStates = append(finalStates, false)
-			}
-		}
-		config := Config{
-			States:      make([]bool, len(finalStates)),
-			FinalStates: finalStates,
-		}
+        finalStates := make([]bool, 0)
+        for _, s := range []rune(parts[0][1 : len(parts[0])-1]) {
+            if s == '#' {
+                finalStates = append(finalStates, true)
+            } else {
+                finalStates = append(finalStates, false)
+            }
+        }
+        config := Config{
+            States:      make([]bool, len(finalStates)),
+            FinalStates: finalStates,
+        }
 
-		buttons := make([]Button, 0)
-		for _, buttonStr := range parts[1 : len(parts)-1] {
-			buttonStr = buttonStr[1 : len(buttonStr)-1]
-			toggledIndicators := make([]int, 0)
-			for _, toggledIndicatorStr := range strings.Split(buttonStr, ",") {
-				toggledIndicator, _ := strconv.Atoi(toggledIndicatorStr)
-				toggledIndicators = append(toggledIndicators, toggledIndicator)
-			}
-			buttons = append(buttons, Button{toggledIndicators})
-		}
-		config.Buttons = buttons
+        buttons := make([]Button, 0)
+        for _, buttonStr := range parts[1 : len(parts)-1] {
+            buttonStr = buttonStr[1 : len(buttonStr)-1]
+            toggledIndicators := make([]int, 0)
+            for _, toggledIndicatorStr := range strings.Split(buttonStr, ",") {
+                toggledIndicator, _ := strconv.Atoi(toggledIndicatorStr)
+                toggledIndicators = append(toggledIndicators, toggledIndicator)
+            }
+            buttons = append(buttons, Button{toggledIndicators})
+        }
+        config.Buttons = buttons
 
-		joltages := make([]int, 0)
-		joltagesStr := parts[len(parts)-1]
-		joltagesStr = joltagesStr[1 : len(joltagesStr)-1]
-		for _, joltageStr := range strings.Split(joltagesStr, ",") {
-			joltage, _ := strconv.Atoi(joltageStr)
-			joltages = append(joltages, joltage)
-		}
-		config.RequiredJoltage = joltages
-		configs = append(configs, config)
-	}
-	return configs
+        joltages := make([]int, 0)
+        joltagesStr := parts[len(parts)-1]
+        joltagesStr = joltagesStr[1 : len(joltagesStr)-1]
+        for _, joltageStr := range strings.Split(joltagesStr, ",") {
+            joltage, _ := strconv.Atoi(joltageStr)
+            joltages = append(joltages, joltage)
+        }
+        config.RequiredJoltage = joltages
+        configs = append(configs, config)
+    }
+    return configs
 }
